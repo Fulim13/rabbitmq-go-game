ea62d301e092ddfad4fe4fa70bc7b3f63c3fcb70
Fu Lim <fulim1130@gmail.com>
2026-09-07 02:22:23 +0800

    connect to rabbitmq server

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# learn-pub-sub-starter (Peril)
+# RabbitMQ
 
-This is the starter code used in Boot.dev's [Learn Pub/Sub](https://learn.boot.dev/learn-pub-sub) course.
+RabbitMQ is the open source message broker that implement amqp protocol
+
+It has 3 components
+
+1. The RabbitMQ server: The message broker itself. It's a server you can run locally (or on a server in production) to send and receive messages.
+2. Client library: We'll be programming in Go, so our code will use the AMQP library to interact with the RabbitMQ server.
+3. Management UI: RabbitMQ comes with a management UI that you can use to monitor and manage your RabbitMQ server. It's a web-based UI that you can access in your browser.
+
+Run this command in a terminal to start RabbitMQ:
+
+```sh
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+```
+
+This will download and run the RabbitMQ server and ManagementUI until you kill it with Ctrl+C
+The server itself is running on port 5672, but you won't interact with it in your browser, just through code. However, you can use the management UI in your browser. Open http://localhost:15672 and log in with the username guest and password guest.
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+// The internal/gamelogic package which contains the game logic for the Peril game.
+// The internal/routing package which contains some routing constants for the game.
+// Stubs of the cmd/client and cmd/server packages, which are main packages that run the client and server for the game.
+// The rabbit.sh script: a convenience for starting and stopping RabbitMQ with Docker. You can run:
+// ./rabbit.sh start to start RabbitMQ
+// ./rabbit.sh stop to stop it
+// ./rabbit.sh logs to view the server logs
+
+// Practice 1:
+// Install the Go AMQP library:
+// go get github.com/rabbitmq/amqp091-go
+
+// Update the cmd/server package's main.go. You'll want to use an alias to import the amqp package:
+// import (
+//     amqp "github.com/rabbitmq/amqp091-go"
+// )
+
+// In the main function, do the following:
+// 1. Declare a connection string with the value: amqp://guest:guest@localhost:5672/. This is how your application will know where to connect to the RabbitMQ server.
+// 2. Call amqp.Dial with the connection string to create a new connection to RabbitMQ.
+// 3. Defer a .Close() of the connection to ensure it's closed when the program exits.
+// 4. Print a message to the console that the connection was successful.
+// 5. Wait for a signal (e.g. Ctrl+C) to exit the program.
+// 6. If a signal is received, print a message to the console that the program is shutting down and close the connection.
+
+// To test your code, use the script to start a RabbitMQ server with Docker in the background:
+// ./rabbit.sh start
+
+// Once it's running, open a new terminal and run your server:
+// go run ./cmd/server
+// You should see your message that the connection was successful. Press Ctrl+C to exit the program and see the shutdown message.
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
